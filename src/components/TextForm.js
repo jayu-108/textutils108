2522b709fe42f18e7196b2048eea03e9016c3b0b
jayu-108 <barijayu@gmail.com>
2023-01-26 20:26:20 +0530

    textutils_improved_deplo_commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -52,16 +52,16 @@ export default function TextForm(props) {
                 color: props.mode==='dark'?'white':'#042743' }}
                 id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To Uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To Lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extraspace</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert To Uppercase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert To Lowercase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpace}>Remove Extraspace</button>
 
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
-            <p> {text.split(" ").length} words and {text.length} characters</p>
+            <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes to read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter Something To Preview It Here"}</p>
